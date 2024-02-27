67ae7f3fde47105079e65a636f58bb4ed317a590
Prasenjit Kushwaha <pk15pi@gmail.com>
2024-02-27 23:06:17 +0530

    step 1.1 and 1.2 done. testing remains

--- actions/step1.py
+++ actions/step1.py
@@ -9,24 +9,43 @@ from .archive_article import Archived_article_attribute
 from .providers import Provider_meta_data_FTP, Provider_meta_data_API
 import datetime
 from io import BytesIO
+import requests
+from django.core.files.base import ContentFile
 
 
+# function to download file
+def download_file(connect, article, item):
+    file_size = connect.size(article)
+    # if record not downloaded in our record and the the file size is not zero than download and write to our database
+    if not (Archived_article_attribute.objects.filter(file_name_on_ftp=article).exists()):
+        content = BytesIO()
+        connect.retrbinary(f'RETR {article}', content.write)
+        content.seek(0)
+        file_type = os.path.splitext(article)[1]
+        x = Archived_article_attribute.objects.create(
+            file_name_on_ftp = article,
+            provider = item.provider.official_name,
+            processed_on = datetime.datetime.today(),
+            status = 'waiting',
+            file_size = file_size,
+            file_type = file_type
+        )
+        x.file_content.save(article, content)
+        return
 
 
-def download_file(ftp, filename, local_path):
-    with open(os.path.join(local_path, filename), 'wb') as f:
-        ftp.retrbinary('RETR ' + filename, f.write)
-
-def download_folder(ftp, folder_name, local_path):
-    os.makedirs(os.path.join(local_path, folder_name), exist_ok=True)
-    ftp.cwd(folder_name)
-    filenames = ftp.nlst()
+# function to iterate folder. If another folder found in side the folder this function will call itself.
+def download_folder(connect, article, item):
+    os.makedirs(os.path.join(article, article), exist_ok=True)
+    connect.cwd(article)
+    filenames = connect.nlst()
     for filename in filenames:
         if '.' in filename:  # It's a file
-            download_file(ftp, filename, os.path.join(local_path, folder_name))
+            download_file(connect, filename, item)
         else:  # It's a subfolder
-            download_folder(ftp, filename, os.path.join(local_path, folder_name))
-    ftp.cwd('..')
+            download_folder(connect, article, os.path.join(article, article))
+    connect.cwd('..')
+    return
 
 
 
@@ -50,7 +69,6 @@ def download_from_ftp(request):
         except Exception as e:
             item.last_pull_time = datetime.datetime.today()
             item.last_pull_status = 'failed'
-            item.next_due_date = datetime.datetime.today() + datetime.timedelta(item.provider.minimum_delivery_fq)
             item.save()
             continue
 
@@ -64,25 +82,13 @@ def download_from_ftp(request):
             for article in article_library:
                 try:
                     # check if the article is file or directory
-                    if not (os.path.isdir(article)):
-                        file_size = connect.size(article)
-                        # if record not downloaded in our record and the the file size is not zero than download and write to our database
-                        if not (Archived_article_attribute.objects.filter(file_name_on_ftp=article).exists()):
-                            content = BytesIO()
-                            connect.retrbinary(f'RETR {article}', content.write)
-                            content.seek(0)
-                            file_type = os.path.splitext(article)[1]
-                            x = Archived_article_attribute.objects.create(
-                                file_name_on_ftp = article,
-                                provider = item.provider.official_name,
-                                processed_on = datetime.datetime.today(),
-                                status = 'waiting',
-                                file_size = file_size,
-                                file_type = file_type
-                            )
-                            x.file_content.save(article, content)
+                    # if not (os.path.isdir(article)):
+                    if '.' in article:
+                        # download the file
+                        download_file(connect, article, item)
                     else:
-                        connect.file
+                        # iterate through the folder and download the files inside it 
+                        download_folder(connect, article, item)
                 except Exception as e:
                     pass
 
@@ -111,54 +117,41 @@ def download_from_api(request):
     # if providers are due to be accessed
     for item in due_for_download:
 
-        # try to connect to FTP, if error occures update the record
-        try:
-            connect = ftplib.FTP(item.server)
-            connect.login(item.account, item.password)
-        except Exception as e:
+        # Send a GET request to the URL
+        response = requests.get(item.base_url)
+        if response.status_code == 200:
+
+            # Retrieve file name and file size from response headers
+            content_disposition = response.headers.get('content-disposition')
+            if content_disposition:
+                file_name = content_disposition.split('filename=')[1]
+            else:
+                file_name = "xx"  # Use URL as filename if content-disposition is not provided
+            file_size = int(response.headers.get('content-length', 0))
+            file_type = os.path.splitext(file_name)[1]
+
+            # Open a local file with write-binary mode and write the content of the response to it
+            x = Archived_article_attribute.objects.create(
+                file_name_on_ftp = file_name,
+                provider = item.provider.official_name,
+                processed_on = datetime.datetime.today(),
+                status = 'waiting',
+                file_size = file_size,
+                file_type = file_type
+            )
+
+            # save file
+            x.file_content.save('filename', ContentFile(response.content))
+
+            # update status
             item.last_pull_time = datetime.datetime.today()
-            item.last_pull_status = 'failed'
+            item.last_pull_status = 'waiting'
             item.next_due_date = datetime.datetime.today() + datetime.timedelta(item.provider.minimum_delivery_fq)
             item.save()
-            continue
+        else:
+            item.last_pull_time = datetime.datetime.today()
+            item.last_pull_status = 'failed'
+            item.save()
 
-        # read the destination location
-
-        connect.cwd(item.site_path)
-        article_library = connect.nlst()
-
-        # if record found explore inside
-        if article_library:
-            for article in article_library:
-                try:
-                    file_size = connect.size(article)
-                    # if record not downloaded in our record and the the file size is not zero than download and write to our database
-                    if not (Archived_article_attribute.objects.filter(file_name_on_ftp=article).exists()):
-                        # with open(article, 'wb') as file:
-                        #     connect.retrbinary(f'RETR {article}', file.write)
-                        content = BytesIO()
-                        connect.retrbinary(f'RETR {article}', content.write)
-                        content.seek(0)
-                        # ftp_file = FTPFile.objects.create(name=article, path=connect.pwd(), size=file_size)
-                        # ftp_file.content.save(article, content)
-                        file_type = os.path.splitext(article)[1]
-                        x = Archived_article_attribute.objects.create(
-                            file_name_on_ftp = article,
-                            provider = item.provider.official_name,
-                            processed_on = datetime.datetime.today(),
-                            status = 'waiting',
-                            file_size = file_size,
-                            file_type = file_type
-                        )
-                        x.file_content.save(article, content)
-                except Exception as e:
-                    pass
-
-        item.last_pull_time = datetime.datetime.today()
-        item.last_pull_status = 'waiting'
-        item.next_due_date = datetime.datetime.today() + datetime.timedelta(item.provider.minimum_delivery_fq)
-        item.save()
-
-        connect.quit()
     return HttpResponse("done")
 
